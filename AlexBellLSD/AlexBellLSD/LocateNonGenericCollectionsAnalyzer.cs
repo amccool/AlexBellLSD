78e2a3d0d815c9e12b33b57d6ad6332af4d4e32c
amccool <amccool@PANTALONE>
2016-11-21 22:15:52 -0800

    Add project files.

---
+++ AlexBellLSD/AlexBellLSD/LocateNonGenericCollectionsAnalyzer.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AlexBellLSD
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class LocateNonGenericCollectionsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "RB-NonGenericCollection";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Design";
+
+        private static DiagnosticDescriptor Rule = 
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, 
+                DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(Rule); 
+            }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.TypeKeyword);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var arrayListType = compilationContext.Compilation.GetTypeByMetadataName("System.Collections.ArrayList");
+
+            var typeInfo = context.SemanticModel.GetTypeInfo(context.Node);
+
+            //if (typeInfo == null)
+            //    return;
+
+            if (typeInfo.Equals(arrayListType))
+            {
+
+                var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ TestProgram/ArrayListsBeHere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram
+{
+    class ArrayListsBeHere
+    {
+        public ArrayList _IhateTheseThings;
+
+        public ArrayList Stilhated { get; set; }
+
+        public ArrayListsBeHere()
+        {
+        }
+
+
+        private void AnotherDisaster()
+        {
+            ArrayList thesesuck = new ArrayList();
+        }
+    }
+}
