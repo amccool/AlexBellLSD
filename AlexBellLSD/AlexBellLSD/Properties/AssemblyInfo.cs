afc895e79a9cc08a4ba31d9fafb687daa04225a4
amccool <alex.mccool@yahoo.com>
2016-11-22 17:28:44 -0800

    nuspec info change

--- AlexBellLSD/AlexBellLSD/Properties/AssemblyInfo.cs
+++ AlexBellLSD/AlexBellLSD/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FindStatics")]
-[assembly: AssemblyDescription("xxx")]
+[assembly: AssemblyTitle("AlexBellLSD")]
+[assembly: AssemblyDescription("AlexBellLSD")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("xxxxx")]
-[assembly: AssemblyProduct("FindStatics")]
+[assembly: AssemblyCompany("AlexBellLSD")]
+[assembly: AssemblyProduct("AlexBellLSD")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
